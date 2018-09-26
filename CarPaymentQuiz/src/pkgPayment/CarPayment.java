3499a4eb8fa5190559a882a8d225abba4a884d11
michaelcerri <michaelcerri@172.18.5.45>
2018-09-25 21:01:09 -0400

    initial commit

---
+++ CarPaymentQuiz/src/pkgPayment/CarPayment.java
@@ -0,0 +1,60 @@
+package pkgPayment;
+
+public class CarPayment {
+	
+	//instantiate variables
+	private double carCost; 
+	private double downPayment;
+	private int loanLength;
+	private double interestRate;
+	
+
+	
+	//constructor
+	public CarPayment(double carCost, double downPayment, int loanLength, double interestRate) {
+		this.carCost = carCost;
+		this.downPayment = downPayment;
+		this.loanLength = loanLength; //in months
+		this.interestRate = interestRate; //interest rate should be input as a double between 0 and 1
+	}
+	
+	//getters and setters
+	public double getCarCost() {
+		return carCost;
+	}
+	public void setCarCost(double carCost) {
+		this.carCost = carCost;
+	}
+	public double getDownPayment() {
+		return downPayment;
+	}
+	public void setDownPayment(double downPayment) {
+		this.downPayment = downPayment;
+	}
+	public int getLoanLength() {
+		return loanLength;
+	}
+	public void setLoanLength(int loanLength) {
+		this.loanLength = loanLength;
+	}
+	public double getInterestRate() {
+		return interestRate;
+	}
+	public void setInterestRate(double interestRate) {
+		this.interestRate = interestRate;
+	}
+	
+	//methods, interest rate compounded monthly using C=cC*(iR*(1+iR)^lL)/((1+iR)^lL-1)
+	
+	public double monthlyCarPayment() {
+		return (carCost*(interestRate/12*Math.pow(1+interestRate/12, loanLength))/(Math.pow(1+interestRate/12, loanLength)-1));
+	}
+	
+	public double interestCost() {
+		return monthlyCarPayment()*loanLength-carCost;
+	}
+	
+	
+
+}
+
---
+++ CarPaymentQuiz/src/pkgPayment/CarPaymentTest.java
@@ -0,0 +1,23 @@
+package pkgPayment;
+
+//junit imports
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class CarPaymentTest {
+	//test monthly
+	@Test
+	public void monthlyCarPaymentTest(){
+		CarPayment cp=new CarPayment(35000,0,60,.10);
+		assertEquals(743.65,cp.monthlyCarPayment(),.01);	
+	}
+	//test interest cost
+	@Test
+	public void interestCostTest(){
+		CarPayment cp=new CarPayment(35000,0,60,.10);
+		assertEquals(9618.79,cp.interestCost(),.01);	
+	}
+
+}
